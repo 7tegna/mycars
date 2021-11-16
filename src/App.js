adf555d6dc7b8b89a12cc3a09c4cae13d973c1f7
Tsegaye Alemayehu <tsegayealemayehu@imac.myfiosgateway.com>
2021-11-16 17:06:39 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import {React, useState } from "react"
 import './App.css';
-
+import Car from "./Components/Car";
+import Cars from "./Cars";
 function App() {
+  const [Car, setCars] = useState(Cars);
+  const handleClear = () => {
+      setCars([]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2> Car List </h2>
+      <h2>{Cars.length} Cars in the lot</h2>
+      <Car car={Car}/>
+      {Car.length !== 0 ? (
+      <button onClick={handleClear}> Clear all</button>
+      ) : (
+      <button onClick={() => setCars(Cars)}>Reset</button>
+      )}
     </div>
   );
 }
